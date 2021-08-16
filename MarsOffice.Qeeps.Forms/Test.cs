0053fdc984a54d7c81cb926ac32129b2a5e04982
Alin Florin <alin@qoffice.ro>
2021-08-17 00:26:42 +0300

    g

--- MarsOffice.Qeeps.Forms/Test.cs
+++ MarsOffice.Qeeps.Forms/Test.cs
@@ -1,32 +1,43 @@
-using System.Net.Http;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using MarsOffice.Qeeps.Microfunction;
+using MarsOffice.Qeeps.Notifications.Abstractions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using MarsOffice.Qeeps.Access.Abstractions;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 
 namespace MarsOffice.Qeeps.Forms
 {
     public class Test
     {
-        private readonly HttpClient _accessClient;
-        public Test(IHttpClientFactory httpClientFactory)
+        public Test()
         {
-            _accessClient = httpClientFactory.CreateClient("access");
         }
 
         [FunctionName("Test")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "api/forms/test")] HttpRequest req)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "api/forms/test")] HttpRequest req,
+            [ServiceBus("notifications", Connection = "sbconnectionstring")] IAsyncCollector<RequestNotificationDto> outputNotifications
+            )
         {
-            var testResponse = await _accessClient.GetStringAsync("/api/access/test");
-            var dto = JsonConvert.DeserializeObject<OrganisationDto>(testResponse, new JsonSerializerSettings {
-                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            var principal = QeepsPrincipal.Parse(req);
+            await outputNotifications.AddAsync(new RequestNotificationDto {
+                AbsoluteRouteUrl = "/test",
+                AdditionalData = new System.Collections.Generic.Dictionary<string, string> {
+                    {"hi", "alin"}
+                },
+                NotificationTypes = new [] {NotificationType.InApp, NotificationType.Email},
+                PreferredLanguage = "en",
+                PlaceholderData = new System.Collections.Generic.Dictionary<string, string> {
+                    {"link", "linkhere"},
+                    {"formName", "formnamehere"}
+                },
+                TemplateName = "FormPosted",
+                RecipientUserIds = new [] {principal.FindFirstValue("id")}
             });
-            return new OkObjectResult(dto);
+            await outputNotifications.FlushAsync();
+            return new OkObjectResult(new {a = 1});
         }
     }
 }
