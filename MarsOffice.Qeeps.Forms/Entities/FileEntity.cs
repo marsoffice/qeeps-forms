153af6dc9b3d51b916a2df6c7c40081daf973380
Alin Florin <alin@qoffice.ro>
2021-08-25 20:58:55 +0300

    -

--- MarsOffice.Qeeps.Forms/Entities/FileEntity.cs
+++
@@ -1,8 +0,0 @@
-namespace MarsOffice.Qeeps.Forms.Entities
-{
-    public class FileEntity
-    {
-        public string Filename { get; set; }
-        public string DownloadLink { get; set; }
-    }
-}
--- MarsOffice.Qeeps.Forms/Entities/FormEntity.cs
+++ MarsOffice.Qeeps.Forms/Entities/FormEntity.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using MarsOffice.Qeeps.Files.Abstractions;
 using Newtonsoft.Json;
 
 namespace MarsOffice.Qeeps.Forms.Entities
@@ -8,12 +9,11 @@ namespace MarsOffice.Qeeps.Forms.Entities
     {
         [JsonProperty("id")]
         public string Id { get; set; }
-        public string AuthorId { get; set; }
-        public string AuthorName { get; set; }
+        public string UserId { get; set; }
         public DateTime CreatedDate { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public IEnumerable<FileEntity> Attachments { get; set; }
+        public IEnumerable<FileDto> Attachments { get; set; }
         public bool IsLocked { get; set; }
         public DateTime? LockedUntilDate { get; set; }
         public bool IsRecurrent { get; set; }
